e9cb43f02618ac3258d93f7eebad63330034db95
alexjab <alexandre.jablon@gmail.com>
2014-06-09 14:32:11 +0200

    complete rewrite of the data structure

--- bin/emberbase.js
+++ bin/emberbase.js
@@ -153,32 +153,24 @@ Emberbase.prototype._initData = function (database) {
 
     db.createReadStream ()
     .on ('data', function (data) {
-    console.log (data);
-      var route = data.key.split ('/')[0];
-      var key = data.key.substr (route.length+1);
+      var key = data.key.split ('/');
+      var route = key.shift ();
+      key = key.join ('/');
       if (!this.__data[route]) {
         this.__data[route] = {};
       }
-      if (key) {
-        this.__data[route][key] = data.value;
-      } else {
-        this.__data[route] = data.value;
+      if (!this._data[route]) {
+        this._data[route] = {};
       }
       if (this.routes.indexOf (route) === -1) {
         this.routes.push (route);
       }
+      this.__data[route][key] = data.value;
     }.bind (this))
     .on ('end', function () {
       Object.keys (this.__data).forEach (function (route) {
-        if (!this._data[route]) {
-          this._data[route] = {};
-        }
-        if (typeof this.__data[route] === 'object') {
-          utils.insertFlatDataSync (this._data[route], this.__data[route]);
-        } else {
-          this._data[route] = this.__data[route];
-        }
-      }, this);
+        utils.inflateObject (this._data, this.__data, route);
+      }.bind (this));
       console.log ('Data loaded from the database');
       this.serverStatus = 'ready';
     }.bind (this))
@@ -195,32 +187,16 @@ Emberbase.prototype._pushData = function (route, commit, callback, context) {
   var path = commit.path;
   var id = utils.generateId (new Date ().getTime ());
 
-  if (this._useDB && !typeof this._data[route] !== 'object') {
-    this._dbWriteStream.write ({type: 'del', key: route+'/'});
-  }
-
-  var data__;
-  if (typeof data === 'object') {
-    data__ = utils.flattenObjectSync ((path?path+'/':'')+id, data);
-    Object.keys (data__).forEach (function (key) {
-      this.__data[route][key] = data__[key];
-      if (this._useDB) {
-        this._dbWriteStream.write ({key: route+'/'+key, value: data__[key]});
-      }
-    }, this);
-  } else {
-    this.__data[route][(path?path+'/':'')+id] = data;
+  utils.flattenData (this.__data, route, path+'/'+id, data, function (type, key, value) {
     if (this._useDB) {
-      this._dbWriteStream.write ({key: route+'/'+(path?path+'/':'')+id, value: data});
+      if (type === 'del') {
+        this._dbWriteStream.write ({type: 'del', key: key});
+      } else {
+        this._dbWriteStream.write ({key: key, value: value});
+      }
     }
-
-    data__ = {};
-    data__[(path?path+'/':'')+id] = data;
-  }
-  if (typeof this._data[route] !== 'object') {
-    this._data[route] = {};
-  }
-  utils.insertFlatDataSync (this._data[route], data__);
+  }.bind (this));
+  utils.inflateObject (this._data, this.__data, route);
 
   callback.call (context, {key: id, val: data}, {val: this._data[route]});
 
@@ -231,47 +207,17 @@ Emberbase.prototype._setData = function (route, commit, callback, context) {
   var data = commit.data;
   var path = commit.path;
 
-  var fullPath = (path?path:'');
-
-  if (typeof this.__data[route] === 'object') {
-    Object.keys (this.__data[route]).forEach (function (key) {
-      if (fullPath === key.substr (0,fullPath.length)) {
-        if (this._useDB) {
-          this._dbWriteStream.write ({type: 'del', key: route+'/'+key});
-        }
-        delete this.__data[route][key];
-      }
-    }, this);
-  } else {
+  utils.flattenData (this.__data, route, path, data, function (type, key, value) {
     if (this._useDB) {
-      this._dbWriteStream.write ({type: 'del', key: route+'/'+this.__data[route]});
-    }
-    this.__data[route] = {};
-  }
-
-  if (typeof data === 'object') {
-    var data__ = utils.flattenObjectSync (fullPath, data);
-    console.log (fullPath, data__);
-    Object.keys (data__).forEach (function (key) {
-      this.__data[route][key] = data__[key];
-      this._dbWriteStream.write ({key: route+'/'+key, value: data__[key]});
-    }, this);
-    this._data[route] = utils.inflateDataSync (data__);
-  } else {
-    if (path) {
-      this.__data[route][path] = data;
-      if (typeof this._data[route] !== 'object') {
-        this._data[route] = {};
+      if (type === 'del') {
+        this._dbWriteStream.write ({type: 'del', key: key});
+      } else {
+        this._dbWriteStream.write ({key: key, value: value});
       }
-      this._data[route][path] = data;
-      this._dbWriteStream.write ({key: route+'/'+path, value: data});
-    } else {
-      this.__data[route] = data;
-      this._data[route] = data;
-      this._dbWriteStream.write ({key: route+'/', value: data});
     }
-  }
-  
+  }.bind (this));
+  utils.inflateObject (this._data, this.__data, route);
+
   callback.call (context, {val: this._data[route]});
   return this;
 };
--- bin/utils.js
+++ bin/utils.js
@@ -1,116 +1,79 @@
-var flattenObject = exports.flattenObject = function (object, init_key, iterator, context) {
-  var flatten = function (long_key, node) {
-    Object.keys (node).forEach (function (key) {
-      var value = node[key];
-      if (typeof value !== 'object') {
-        iterator.call (context, long_key+'.'+key, value);
-      } else {
-        flatten (long_key+'.'+key, value);
-      }
-    });
-  };
-  flatten (init_key?'.'+init_key:'', object);
-};
-
-var flattenObjectSync = exports.flattenObjectSync = function (initKey, object) {
-  var flatObject = {};
-  var flatten = function (longKey, node) {
-    Object.keys (node).forEach (function (key) {
-      var value = node[key];
-      if (typeof value !== 'object') {
-        flatObject[longKey+'/'+key] = value;
-      } else {
-        flatten (longKey+'/'+key, value);
-      }
-    });
-  };
-  flatten (initKey?initKey:'', object);
-  return flatObject;
-};
-
-var insertFlatDataSync = exports.insertFlatDataSync = function (object, flatObject) {
-  var insert = function (object, longKey, value) {
-    var key = longKey.shift ();
-    if (longKey.length === 0) {
-      object[key] = value;
-    } else {
-      if (typeof object[key] !== 'object') {
-        object[key] = {};
-      }
-      insert (object[key], longKey, value);
-    }
-  };
-  console.log (flatObject);
-  Object.keys (flatObject).forEach (function (key) {
-    console.log (key);
-    var longKey;
-    if (key.indexOf ('/') === -1) {
-      longKey = [key];
-    } else {
-      longKey = key.split ('/');
-    }
-    insert (object, longKey, flatObject[key]);
-  });
-};
-
-var inflateDataSync = exports.inflateDataSync = function (flatObject) {
-  var inflate = function (object, longKey, value) {
-    var key = longKey.shift ();
-    if (longKey.length === 0) {
-      if (typeof value !== 'object') {
+var setReference = function (object, route, longKey, value) {
+  if (longKey === '/') {
+    object[route] = value;
+  } else {
+    var set = function (object, longKey, value) {
+      var key = longKey.shift ();
+      if (longKey.length === 0) {
         object[key] = value;
-      }
-    } else {
-      if (!object[key]) {
-        object[key] = {};
-      }
-      inflate (object[key], longKey, value);
-    }
-  };
-
-  var object = {};
-  Object.keys (flatObject).forEach (function (key) {
-    inflate (object, key.split ('/'), flatObject[key]);
-  });
-  return object;
-};
-
-var mergeObjects = exports.mergeObjects = function (host, guest) {
-  var merge = function (host, guest) {
-    Object.keys (guest).forEach (function (key) {
-      var val = guest[key];
-      if (typeof val !== 'object') {
-        host[key] = val;
       } else {
-        merge (host[key] = {}, val);
+        if (typeof object[key] !== 'object') {
+          object[key] = {};
+        }
+        set (object[key], longKey, value);
       }
-    });
+    };
+    if (typeof object[route] !== 'object') {
+      object[route] = {};
+    }
+    set (object[route], longKey.split ('/'), value);
   }
-  merge (host, guest);
-  return host;
 };
 
-var inflateObject = exports.inflateObject = function (flatObject) {
-  var inflate = function (object, longKey, value) {
-    var key = longKey.shift ();
-    if (longKey.length === 0) {
-      if (typeof value !== 'object') {
-        object[key] = value;
-      } else {
-        object[key] = mergeObjects ({}, value);
-      }
-    } else {
-      if (!object[key])
-        object[key] = {};
-      inflate (object[key], longKey, value);
-    }
-  };
-
-  var object = {};
-  Object.keys (flatObject).forEach (function (key) {
-    inflate (object, key.replace ('.', '').split ('.'), flatObject[key]);
+var inflateObject = exports.inflateObject = function (store, flatObject, route) {
+  Object.keys (flatObject[route]).forEach (function (key) {
+    setReference (store, route, key, flatObject[route][key]);
   });
-  return object;
+}
+
+var flattenData = exports.flattenData = function (flatObject, route, path, data, iterator) {
+  if (typeof data !== 'object') {
+    if (flatObject[route]['/']) {
+      delete flatObject[route]['/'];
+      iterator.call (null, 'del', route+'/');
+    } else {
+      Object.keys (flatObject[route]).forEach (function (key) {
+        if ((path && key.substr (0, path.length) === path) || !path) {
+          delete flatObject[route][key];
+          iterator.call (null, 'del', route+'/'+key);
+        }
+      });
+    }
+    if (!path) {
+      flatObject[route]['/'] = data;
+      iterator.call (null, 'put', route+'/', data);
+    } else {
+      flatObject[route][path] = data;
+      iterator.call (null, 'put', route+'/'+path, data);
+    }
+  } else {
+    var flatten = function (path, data) {
+      if (typeof data === 'object') {
+        Object.keys (data).forEach (function (key) {
+          if (!path) {
+            flatten (key, data[key]);
+          } else {
+            flatten (path+'/'+key, data[key]);
+          }
+        });
+      } else {
+        if (flatObject[route]['/']) {
+          delete flatObject[route]['/'];
+          iterator.call (null, 'del', route+'/');
+        } else {
+          Object.keys (flatObject[route]).forEach (function (key) {
+            if (key.substr (0, path.length) === path) {
+              delete flatObject[route][key];
+              iterator.call (null, 'put', route+'/'+key);
+            }
+          });
+        }
+        flatObject[route][path] = data;
+        iterator.call (null, 'put', route+'/'+path, data);
+      }
+    };
+    flatten (path, data);
+  }
 };
 
 var generateId = exports.generateId = function (seed) {
